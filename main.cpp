d6b3e0ba44b9d61db6cfa5d95cc23dea41100560
CoolManE4 <95052310+CoolManE4@users.noreply.github.com>
2022-04-18 01:17:22 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "convert.h"
+
+void doCommand(string command) {
+  if (command.find("disassemble")) {
+    
+  }
+}
+
+int main() {
+  while (true) {
+    string command;
+    
+    cin >> command;
+    
+    doCommand(command);
+  }
+}
