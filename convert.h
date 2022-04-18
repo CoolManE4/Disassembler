6dd5ad601652abec7c91efbd50cd333e342addac
CoolManE4 <95052310+CoolManE4@users.noreply.github.com>
2022-04-18 01:06:42 -0400

    Shortened TODO list

--- convert.h
+++ convert.h
@@ -2,10 +2,6 @@
 #include <string>
 
 // TODO: I need to add toHex function which will convert an integer to a hexadecimal value
-// I also need to add toOpcode which will convert a hexadecimal value over to an opcode
-// After that I need to add a function called toRegister that looks inside the hexadecimal to see if its eax,ebx,ecx,edx etc
-// I also need to add a arrayToString function which converts an array to a string like this array [1,0] would be converted into "10"
-// I also need to finish up the toAscii function
 
 // HOW IT WILL WORK: it will work by turning the text inside the file to ASCII then to Hexadecimal
 // then after its been turned to hexadecimal it fires the toOpcode function and toRegister function on the text and they check if
