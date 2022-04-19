c928535baf3e666e0ae452a90a99df996a52523c
CoolManE4 <95052310+CoolManE4@users.noreply.github.com>
2022-04-19 02:06:32 -0400

    Added more functions and changed Opcodes class

--- convert.h
+++ convert.h
@@ -21,14 +21,14 @@ using namespace std;
 class Opcodes {
   public:
     const int CALL_OPCODE;
+  
+    const int RET_OPCODE = "C3";
 
-    const int PUSH_OPCODE;
-    const int POP_OPCODE;
+    const int PUSH_OR_POP_OPCODE = "665";
 
-    const int INC_OPCODE;
-    const int DEC_OPCODE;
-    const int ADD_OPCODE;
-    const int SUB_OPCODE;
+    const int INC_OR_DEC_OPCODE = "664";
+  
+    const int ADD_OR_SUB_OPCODE = "6683";
 };
 
 class Registers {
@@ -36,7 +36,7 @@ class Registers {
     const int EAX_REGISTER;
     const int EBX_REGISTER;
     const int ECX_REGISTER;
-    const int EDX_REGISTER
+    const int EDX_REGISTER;
   
     const int AX_REGISTER;
     const int BX_REGISTER;
@@ -59,7 +59,38 @@ string disassemble(string fileDirectory) {
   toRegister(hexadecimal);
 }
 
-string arrayToString(int array) {
+int reverseArray(int array[256]) {
+  int arrayLength = *(&array + 1) - array;
+  
+  int reversedArray[256];
+  
+  int i = arrayLength;
+  
+  int index = 0;
+  
+  while (i != 0) {
+    reversedArray[i] = array[i];
+    
+    index++;
+    i--;
+  }
+  
+  return reversedArray;
+}
+
+int getPosition(string text,string findInText) {
+  int length = findInText.length();
+  
+  int begin = text.find(findInText);
+  
+  int end = (begin * length) + 1;
+  
+  int position[2] = {begin, end};
+  
+  return position;
+}
+
+string intArrayToString(int array) {
   string packedArray = "";
   
   int arrayLength = *(&array + 1) - array;
@@ -73,6 +104,30 @@ string arrayToString(int array) {
   }
   
   return packedArray;
+}
+
+string packStringArray(string array)
+  string packedStringArray = "";
+  
+  int arrayLength = *(&array + 1) - array;
+
+  int i = 0;
+
+  while (i < arrayLength) {
+    packedStringArray = packedStringArray + array[i];
+    
+    i++;
+  }
+
+  return packedStringArray;
+}
+
+string replace_all(string text,string replaced,string replaceWith) {
+  int position = getPosition(text,replaced);
+  
+  int begin = position[0];
+  
+  int end = position[1];
 }
 
 int toAscii(string text) {
@@ -90,9 +145,59 @@ int toAscii(string text) {
     i++;
   }
   
-  string packedAsciiArray = arrayToString(asciiArray);
+  string packedAsciiArray = intArrayToString(asciiArray);
   
   return packedAsciiArray;
+}
+
+string arrayToHex(int array) {
+  int arrayLength = *(&array + 1) - array;
+  
+  string stringArray[arrayLength];
+  
+  int i = 0;
+  
+  while (i < arrayLength) {
+    int decimal = array[i];
+    
+    if (decimal == 10) stringArray[i] = "A";
+    if (decimal == 11) stringArray[i] = "B";
+    if (decimal == 12) stringArray[i] = "C";
+    if (decimal == 13) stringArray[i] = "D";
+    if (decimal == 14) stringArray[i] = "E";
+    if (decimal == 15) stringArray[i] = "F";
+    
+    i++;
+  }
+  
+  string packedStringArray = packStringArray(stringArray);
+  
+  return stringArray;
+}
+
+// Hexadecimal conversion method taken from https://www.tutorialspoint.com/how-to-convert-decimal-to-hexadecimal
+string toHex(int decimal) {
+  int divide = decimal / 16;
+  int remainder = decimal % 16;
+  
+  int array[256];
+  
+  int i = 0;
+  
+  while (divide != 0) {
+    i++;
+    
+    divide = divide / 16;
+    remainder = remainder % 16;
+    
+    array[i] = remainder;
+  }
+  
+  int reversedArray = reverseArray(array);
+  
+  string hexadecimal = arrayToHex(reversedArray);
+  
+  return hexadecimal;
 }
 
 string toRegister(string hexadecimal) {
